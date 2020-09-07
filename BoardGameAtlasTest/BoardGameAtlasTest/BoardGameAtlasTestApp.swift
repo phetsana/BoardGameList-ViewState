0276945795319e07b9852e220e150b96c1637d34
Phetsana Phommarinh <phetsana.phommarinh@orange.com>
2020-09-07 23:16:21 +0200

    Initial project

---
+++ BoardGameAtlasTest/BoardGameAtlasTest/BoardGameAtlasTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoardGameAtlasTestApp.swift
+//  BoardGameAtlasTest
+//
+//  Created by Phetsana PHOMMARINH on 07/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct BoardGameAtlasTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
